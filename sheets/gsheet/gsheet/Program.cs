8d35c6ad5ac92586d2b44d824c8b8652c17af97d
nguyenvanthinh <nguyenvanthinh@f88.vn>
2020-05-20 17:30:54 +0700

    a

--- sheets/gsheet/gsheet/Program.cs
+++ sheets/gsheet/gsheet/Program.cs
@@ -7,18 +7,29 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Threading;
+using Newtonsoft.Json;
+
+// https://developers.google.com/sheets/api/quickstart/dotnet
+// https://medium.com/@williamchislett/writing-to-google-sheets-api-using-net-and-a-services-account-91ee7e4a291
 
 namespace gsheet
 {
     class Program
     {
-        // If modifying these scopes, delete your previously saved credentials
-        // at ~/.credentials/sheets.googleapis.com-dotnet-quickstart.json
-        static string[] Scopes = { SheetsService.Scope.SpreadsheetsReadonly };
-        static string ApplicationName = "Google Sheets API .NET Quickstart";
 
         static void Main(string[] args)
         {
+            //test_01();
+            test_02();
+        }
+
+        static void test_01()
+        {
+            // If modifying these scopes, delete your previously saved credentials
+            // at ~/.credentials/sheets.googleapis.com-dotnet-quickstart.json
+            string[] Scopes = { SheetsService.Scope.SpreadsheetsReadonly };
+            string ApplicationName = "Google Sheets API .NET Quickstart";
+
             UserCredential credential;
 
             using (var stream = new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
@@ -55,6 +66,73 @@ namespace gsheet
                     // Print columns A and E, which correspond to indices 0 and 4.
                     Console.WriteLine("{0}, {1}", row[0], row[4]);
                 }
+            }
+            else
+            {
+                Console.WriteLine("No data found.");
+            }
+
+            Console.Read();
+        }
+
+        static void test_02()
+        {
+            // If modifying these scopes, delete your previously saved credentials
+            // at ~/.credentials/sheets.googleapis.com-dotnet-quickstart.json
+            string[] Scopes = { SheetsService.Scope.SpreadsheetsReadonly };
+            string ApplicationName = "Google Sheets API .NET Quickstart";
+
+            UserCredential credential;
+
+            using (var stream = new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
+            {
+                // The file token.json stores the user's access and refresh tokens, and is created
+                // automatically when the authorization flow completes for the first time.
+                string credPath = "token.json";
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(GoogleClientSecrets.Load(stream).Secrets, Scopes, "user", CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }
+
+            // Create Google Sheets API service.
+            var service = new SheetsService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+
+
+            // https://docs.google.com/spreadsheets/d/1NuiucytoqquLHUdLiuJDR71ddHcyZWGfTtvI-uiXhxI/edit?usp=sharing
+
+            String spreadsheetId = "1NuiucytoqquLHUdLiuJDR71ddHcyZWGfTtvI-uiXhxI";
+            String range = "T5.25-31!A8:K";
+            SpreadsheetsResource.ValuesResource.GetRequest request = service.Spreadsheets.Values.Get(spreadsheetId, range);
+            //SpreadsheetsResource.ValuesResource.GetRequest request = service.Spreadsheets.Values.Get(spreadsheetId, "T5.25-31");
+
+            IList<IList<Object>> values = null;
+            try
+            {
+                ValueRange response = request.Execute();
+                values = response.Values;
+            }
+            catch (Exception exx) {
+                string errr = exx.Message;
+                // ERROR: Google.Apis.Requests.RequestError This operation is not supported for this document [400] Errors [ 	Message[This operation is not supported for this document] Location[ - ] Reason[failedPrecondition] Domain[global] ]
+                // -> Open docs.google.com//spreadsheets/d/... -> File -> Save as Google Sheets
+            }
+
+            if (values != null && values.Count > 0)
+            {
+                //Console.WriteLine("Name, Major");
+                //foreach (var row in values)
+                //{
+                //    // Print columns A and E, which correspond to indices 0 and 4.
+                //    Console.WriteLine("{0}, {1}", row[0], row[4]);
+                //}
+
+                string json = JsonConvert.SerializeObject(values, Formatting.Indented);
+                Console.WriteLine(json);
+
             }
             else
             {
