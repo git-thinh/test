16ebd7d8e1c31205a583059cb37c7043d2970a9d
nguyenvanthinh <nguyenvanthinh@f88.vn>
2020-05-29 21:44:10 +0700

    a

--- web/_site/admin/_view/mar88/msg_template.js
+++ web/_site/admin/_view/mar88/msg_template.js
@@ -16,6 +16,37 @@
         //    _self.f_table_draw();
         //});
 
+        var vue_table = _self.$refs['VUE_KIT_TABLE'];
+        //console.log(vue_table);
+        if (vue_table) {
+            var obj = {
+                id: 'Mã tin',
+                str_group: 'Nhóm tin',
+                str_subject: 'Chủ đề tin',
+                str_content: 'Nội dung tin',
+                str_action: 'Thao tác'
+            };
+
+            //vue_table.f_table_draw_empty(obj);
+            vue_table.f_table_draw_loading(obj);
+
+            setTimeout(function () {
+                var arr_items = [];
+                var options = {
+                    fixed_left_column: 1,
+                    fixed_right_column: 0
+                };
+
+                for (var i = 0; i < 1000; i++) {
+                    var cols = Object.keys(obj), ait = [];
+                    for (var j = 0; j < cols.length; j++) ait.push(j + 1);
+                    arr_items.push(ait);
+                }
+
+                vue_table.f_table_draw(obj, arr_items, options);
+
+            }, 1000);
+        }
     },
     methods: {
     }
