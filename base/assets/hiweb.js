0c06b75d71ff4f4c24a9f1c11c3960aade78f189
Mr Thinh <33323583+git-thinh@users.noreply.github.com>
2020-06-16 20:32:34 +0700

    a

--- base/assets/hiweb.js
+++ base/assets/hiweb.js
@@ -1,50 +1,134 @@
-﻿const Hiweb = (function () {
+﻿var Hiweb = (function () {
     function HiwebObject(options) {
         options = options || {};
+        var _APP, _DATA = {
+            view: {
+                loading: null,
+                login: null,
+                confirm_pass: null,
+                screen_lock: null,
 
+                sidebar_left: null,
+                sidebar_right: null,
 
-        this.name = "SingletonTester";
-        this.pointX = options.pointX || 6;
-        this.pointY = options.pointY || 10;
+                header_top: null,
+                header_breadcrumb: null,
+                header_tab: null,
+                header_filter: null,
+                main_left: null,
+                main_body: null,
+                footer: null,
 
+                dialog_1: null,
+                dialog_2: null,
+                dialog_3: null,
 
-        // Singleton
+                popup_1: null,
+                popup_2: null,
+                popup_3: null,
 
-        // Private methods and variables
-        function privateMethod() {
-            console.log("I am private");
-        }
+                alert_1: null,
+                alert_2: null,
+                alert_3: null
+            }
+        };
 
-        var privateVariable = "Im also private";
+        function _getUrl(url, typeResponse) {
+            typeResponse = typeResponse || 'text';
 
-        var privateRandomNumber = Math.random();
+            var xhr = new XMLHttpRequest();
+            xhr.open('GET', url, false);
+            xhr.send(null);
 
-        return {
+            var result = '';
+            if (typeResponse != 'text') result = null;
 
-            // Public methods and variables
-            publicMethod: function () {
-                console.log("The public can see me!");
-            },
-
-            publicProperty: "I am also public",
-
-            getRandomNumber: function () {
-                return privateRandomNumber;
+            if (xhr.status === 200) {
+                if (typeResponse == 'text') result = xhr.responseText;
+                else result = JSON.parse(xhr.responseText);
             }
 
+            return result;
+        }
+
+        function _vueInit(cb) {
+            _APP = new Vue({
+                el: '#app',
+                data: function () { return _DATA; },
+                mounted: function () {
+                    Vue.nextTick(function () {
+                        if (cb) cb();
+                    });
+                }
+            });
+        }
+
+        function _appInit() {
+            var cf = _getUrl('app/config.json', 'json');
+            var cms = _getUrl('app/list.json', 'json');
+            if (cf && cms) {
+                _vueInit(() => {
+                    if (cf.loading) {
+                        var cfLoading = _.find(cms, function (o) { return o.key == cf.loading; });
+                        if (cfLoading) {
+                            //_APP.view.loading 
+
+                            var files = cfLoading.files;
+                            var fets = [];
+                            for (var i = 0; i < files.length; i++)
+                                fets.push(fetch(cfLoading.scope + '/' + cfLoading.name + '/' + files[i]));
+                        }
+                    }
+                });
+            } else {
+                console.error('Cannot found the file: app/config.json or app/list.json');
+            }
+        }
+
+        return {
+            getApp: function () { return _APP; },
+            init: function (libs) {
+                libs = libs || [];
+                if (libs.length > 0) {
+                    var items = [];
+                    for (var i = 0; i < libs.length; i++) {
+                        var item = {};
+                        item['item_' + i] = 'assets/libs/' + libs[i] + '?v='+ new Date().getTime();
+                        items.push(item);
+                    }
+                    head.load(items, _appInit);
+                }
+            },
+            mainLoading: function (visiable) {
+                var el = document.getElementById('main_loading');
+                if (el) {
+                    if (visiable == false)
+                        el.style.display = 'none';
+                    else
+                        el.style.display = 'inline-block';
+                }
+            }
         };
     }
 
     var instance;
     return {
         getInstance: function (options) {
-            if (instance === undefined) {
-                instance = new HiwebObject(options);
-            }
+            if (instance === undefined) instance = new HiwebObject(options);
             return instance;
         }
     };
 })();
 
 
-const hiw = mySingleton.getInstance();
+const hiw = Hiweb.getInstance();
+hiw.init([
+    'jquery-3.5.1.slim.min.js',
+    'popper.min.js',
+    'lodash.min.js',
+    'classie.js',
+    'vue.min.js',
+    'bootstrap.min.js',
+    'bootstrap.min.css',
+    'reset.css'
+]);
